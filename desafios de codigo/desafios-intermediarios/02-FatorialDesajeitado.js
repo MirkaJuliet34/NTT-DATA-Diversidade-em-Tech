65b4357c8c6e15b5d759918858845f429b8dfc56
Aline Sanches <96800792+alnsanches@users.noreply.github.com>
2022-10-24 16:41:52 -0300

    Update 02-FatorialDesajeitado.js

--- desafios de codigo/desafios-intermediarios/02-FatorialDesajeitado.js
+++ desafios de codigo/desafios-intermediarios/02-FatorialDesajeitado.js
@@ -1,38 +1,63 @@
+// IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente, onde:  
+// - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário; 
+// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente. 
+
+
 let lines = gets().split("\n"); 
 let n = parseInt(lines.shift()); 
 
-if (n <= 2) { 
-  n * 1;
-  print(n); 
-} else {
-  
-  let res = 0;
-  let cnt = 1;
-  let tmp = n;
-  n--;
 
-  while (n > 0) {
+// TODO: Calcule o valor do fatorial de "n", atribuindo o retorno a "r".
+let i = n;
+let arr = [];
+let total = 0;
 
-  tmp *= n;
-  n--;
 
-  if (n != 0) {
-    tmp /= n;
-    n--;
-  }
-
-  tmp += n;
-  n--;
-
-  res += tmp;
-
-  tmp = -n;
-
-  if (n == 1 ) {
-    res += tmp;
-  }   
-
-  n--;
+while (i >= 1) {
+    arr[i] = i
+    i--
 }
-  print(res.toFixed(0));
-} 
+arr.shift()
+let array = arr.reverse()
+let a = array.length
+
+
+while (array.length > 0) {
+    if (array.length >= 4) {
+        total += parseInt((array[0] * array[1]) / (array[2])) + array[3]
+        array.splice(0, 4)        
+        while (array.length >= 4) {
+            total = total - parseInt((array[0] * array[1]) / array[2]) + array[3]
+            array.splice(0, 4)
+        }
+        if (array.lengh == 3) {
+            total = total - parseInt((array[0] * array[1]) / array[2])
+            array.splice(0, 3)
+        }
+        if (array.length == 2) {
+            total = total - (array[0] * array[1])
+            array.splice(0, 2)
+        }
+        if (array.length == 1) {
+            total = total - array[0]
+            array.splice(0, 1)
+        }
+    }
+    if (array.lengh == 3) {
+        total = total + parseInt((array[0] * array[1]) / array[2])
+        array.splice(0, 3)
+        if (array.length == 2) {
+            total = total - (array[0] * array[1])
+            array.splice(0, 2)
+        }
+        if (array.length == 1) {
+            total = total - array[0]
+            array.splice(0, 1)
+        }
+    }
+    if (array.length == 2) {
+        total = total - (array[0] * array[1])
+        array.splice(0, 2)
+    }
+}
+print(parseInt(total));
